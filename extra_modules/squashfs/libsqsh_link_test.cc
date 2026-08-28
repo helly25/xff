072832b4784e7dfbbe31cd2c0e46e89dd74ec88a
helly25 <6420169+helly25@users.noreply.github.com>
2026-08-28 02:23:34 +0100

    test: use only libsqsh public API

--- extra_modules/squashfs/libsqsh_link_test.cc
+++ extra_modules/squashfs/libsqsh_link_test.cc
@@ -7,28 +7,16 @@
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-#include "libsqsh_codec_probe.h"
 
 namespace xff::squashfs {
 namespace {
 
 using ::testing::HasSubstr;
-using ::testing::IsFalse;
-using ::testing::IsTrue;
 
 struct LibsqshLinkTest : ::testing::Test {};
 
 TEST_F(LibsqshLinkTest, ExposesVersionedLibrary) {
   EXPECT_THAT(std::string_view(sqsh_version()), HasSubstr("1.5.2"));
-}
-
-TEST_F(LibsqshLinkTest, LinksEveryPermissivelyLicensedSquashfsCodecButNotLzo) {
-  EXPECT_THAT(xff_libsqsh_has_extractor(SQSH_COMPRESSION_GZIP), IsTrue());
-  EXPECT_THAT(xff_libsqsh_has_extractor(SQSH_COMPRESSION_LZMA), IsTrue());
-  EXPECT_THAT(xff_libsqsh_has_extractor(SQSH_COMPRESSION_XZ), IsTrue());
-  EXPECT_THAT(xff_libsqsh_has_extractor(SQSH_COMPRESSION_LZ4), IsTrue());
-  EXPECT_THAT(xff_libsqsh_has_extractor(SQSH_COMPRESSION_ZSTD), IsTrue());
-  EXPECT_THAT(xff_libsqsh_has_extractor(SQSH_COMPRESSION_LZO), IsFalse());
 }
 
 }  // namespace
